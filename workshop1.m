c9376ed3393aa513f97d935ba96f65d4a1bc829b
Moinak Das <52611169+Sadkoi@users.noreply.github.com>
2022-09-16 15:26:56 -0400

    Update workshop1.m

--- workshop1.m
+++ workshop1.m
@@ -16,25 +16,3 @@ for ii = 1:length(input)
 end
 
 disp(enc);
-
-
-
-
-
-
-
-
-
-%dec = '';
-%for jj = 1:3
-%   if(enc(jj) == ' ')
-%        dec = dec + ' ';
-%   else
-%       if(strfind(alphabet,enc(jj)) < shiftFactor)
-%           dec = dec + alphabet(26 - (shiftFactor - strfind(alphabet,enc(jj))));
-%           disp(26 - (shiftFactor - strfind(alphabet,enc(jj))));
-%       else
-%            dec = dec + alphabet(strfind(alphabet,enc(jj)) - shiftFactor);
-%       end
-%   end
-%end
